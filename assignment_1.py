2ff6d369c504fe4163469ff279d8e5a0c7fccb86
BaqerMun <mrbaqermo@gmail.com>
2026-01-31 11:50:47 +0300

    The assignment_1 python file with Baqer's comment

---
+++ assignment_1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pdb
+import xarray as xr
+dset = xr.open_dataset(r"C:\Users\mousabm\Downloads\SRTMGL1_NC.003_Data\N21E039.SRTMGL1_NC.nc")
+pdb.set_trace()
+#Comment: This code is part of an assignment given in the goe-environmental analysis course. 
