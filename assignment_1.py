975804ab6503a179e009acef8a460272b15ad738
BaqerMun <mrbaqermo@gmail.com>
2026-01-31 12:00:58 +0300

    Added code to visualize Jeedah's topography

--- assignment_1.py
+++ assignment_1.py
@@ -5,4 +5,4 @@ import pdb
 import xarray as xr
 dset = xr.open_dataset(r"C:\Users\mousabm\Downloads\SRTMGL1_NC.003_Data\N21E039.SRTMGL1_NC.nc")
 pdb.set_trace()
-#Comment: This code is part of an assignment given in the goe-environmental analysis course. 
+#Comment: This code is part of an assignment given in the goe-environmental analysis course and it is the first assignment in the course. 
